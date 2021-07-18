f8950a7a3f4419fad70b44875940a61bc0153def
Tatyana <anatoliy.gromov.service@gmail.com>
2021-07-18 19:34:32 +0300

    4. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,21 +1,54 @@
 package ru.job4j.concurrent;
 
-public class Wget {
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
 
-    public static void main(String[] args) {
-        Thread threadLoad = new Thread(
-                () -> {
-                    try {
-                        for (int i = 0; i < 100; i++) {
-                            System.out.print("\rLoading : " + i  + "%");
-                            Thread.sleep(1000);
+public class Wget implements Runnable {
+
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try {
+            try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+                 FileOutputStream fileOutputStream = new FileOutputStream("src/main/resources/tmp.file")) {
+                    byte[] dataBuffer = new byte[1024];
+                    int bytesRead;
+                    int count = 0;
+                    while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                        long startTime = System.currentTimeMillis();
+                        count++;
+                        String s = count % 2 == 0 ? " " :  "wait";
+                        System.out.print("\r loading: " + s);
+                        fileOutputStream.write(dataBuffer, 0, bytesRead);
+                        long endTime = System.currentTimeMillis();
+                        long rslTime = endTime - startTime;
+                        if (speed > rslTime) {
+                            Thread.sleep(speed - rslTime);
                         }
-                        System.out.print("\rLoading is complete.");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
                     }
-                }
-        );
-        threadLoad.start();
+                System.out.print("\rLoading is complete.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }
