fe1be2fb000835d82516f3903aa893f63438dfbc
Tatyana <anatoliy.gromov.service@gmail.com>
2021-07-26 21:07:50 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/concurrent/wait/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/concurrent/wait/SimpleBlockingQueueTest.java
@@ -3,7 +3,11 @@ package ru.job4j.concurrent.wait;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -65,5 +69,80 @@ public class SimpleBlockingQueueTest {
         first.join();
         second.join();
         assertThat(second.rsl, is(input));
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        Thread producer = new Thread(
+                () -> {
+                    try {
+                        for (int i = 0; i < 5; i++) {
+                            queue.offer(i);
+                        }
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
+    }
+
+    @Test
+    public void whenProducerParseStringThenConsumerGetIt() {
+        String input = "Test for blocking queue";
+        final CopyOnWriteArrayList<Character> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Character> queue = new SimpleBlockingQueue<>(input.length());
+        Thread producer = new Thread(
+                () -> {
+                        input.chars().mapToObj(value -> (char) value).forEach(character -> {
+                            try {
+                                queue.offer(character);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        });
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        try {
+            producer.join();
+            consumer.interrupt();
+            consumer.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        List<Character> expected = input.chars().mapToObj(
+                value -> (char) value).collect(Collectors.toList());
+        assertThat(buffer, is(expected));
     }
 }
