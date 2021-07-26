2543ce288a1d3afac15d1f58ed03b2d48bcc1e1f
Tatyana <anatoliy.gromov.service@gmail.com>
2021-07-26 22:21:59 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/concurrent/nonblocking/CASCount.java
@@ -0,0 +1,21 @@
+package ru.job4j.concurrent.nonblocking;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class CASCount {
+
+    private final AtomicReference<Integer> count = new AtomicReference<>(0);
+
+    public void increment() {
+        Integer expected;
+        Integer newVal;
+        do {
+            expected = count.get();
+            newVal = expected++;
+        } while (!count.compareAndSet(expected, newVal));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
